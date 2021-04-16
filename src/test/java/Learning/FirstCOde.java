e54ed53e2342d8e7a060b631ece5398d2c722212
khedidjaN <nemrikhadidja@gmail.com>
2021-04-16 12:57:26 -0400

    First code

--- src/test/java/Learning/FirstCOde.java
+++ src/test/java/Learning/FirstCOde.java
@@ -3,5 +3,6 @@ package Learning;
 public class FirstCOde {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("HI again");
     }
 }
