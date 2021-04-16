d20e57b67e9e409475d28481ad8f8bb60c263f33
khedidjaN <nemrikhadidja@gmail.com>
2021-04-16 13:07:26 -0400

    Second code

--- src/test/java/Learning/FirstCOde.java
+++ src/test/java/Learning/FirstCOde.java
@@ -4,5 +4,6 @@ public class FirstCOde {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("HI again");
+        System.out.println("Hi you again");
     }
 }
